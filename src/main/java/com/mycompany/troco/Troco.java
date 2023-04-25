4e0ca38f89f20304efa03cb5d3a50493f73b46ab
Gabriel Ricardo <93540978+gabrielricardosilva@users.noreply.github.com>
2023-04-25 13:37:12 -0300

    troco em java

---
+++ src/main/java/com/mycompany/troco/Troco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.troco;
+
+/**
+ *
+ * @author gabri
+ */
+public class Troco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
